e6fcc04f1a2572ccc6fac8b08aad4c73a68894c3
Bharat <mishrabharat1993@gmail.com>
2020-05-01 12:14:39 +0530

    added overlay for desktop

--- js/main.js
+++ js/main.js
@@ -22,10 +22,10 @@ function toggleMenu() {
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
-    menu.classList.remove("close");
-    menuNav.classList.remove("close");
-    menuBranding.classList.remove("close");
-    navItems.forEach((item) => item.classList.remove("close"));
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach((item) => item.classList.remove("show"));
 
     // Set Menu State
     showMenu = false;
